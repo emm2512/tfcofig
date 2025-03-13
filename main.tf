cf14d4ef6470aea8e2558a75e378c150e6b6dd05
emm2512 <emmamound2@gmail.com>
2025-03-13 08:47:47 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,3 +11,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "jenkins-terra" "ec2" {
+    ami = "ami-04aa00acb1165b32a"
+    instance_type = "t2.micro"
+    key_pair_name = "resume"
+    availability_zone = "us-east-1a"
+    tags {
+        Name = "myproject"
+        env = "dev"
+    }
+
+}
